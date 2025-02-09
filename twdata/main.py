b45dfbf65b75af0823f34d491beaa7a55858c7cb
Floris <florisvanrossum@gmail.com>
2025-02-09 15:51:04 -0500

    updates to worlds to download

--- twdata/main.py
+++ twdata/main.py
@@ -1,10 +1,13 @@
 from twapi import TWAPI
 
 # List of world numbers to download
-worlds = [143, 144, 145]
+worlds = [142, 143, 144, 145]
 
 # Create an instance of the TWAPI class
 tw = TWAPI(worlds, "en")
 
+tw_classic = TWAPI([1, 2], "enc")
+
 # Download the files
 tw.get_files()
+tw_classic.get_files()
